1648c8f6cdf7a19ea4712d871fb8b127c408da02
Scary Monsters_ <126077368+nice-sprites@users.noreply.github.com>
2023-03-21 14:00:51 -0300

    Initial commit

---
+++ start.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+@client.event
+async def on_ready():
+    print('bot is ready')
+
+
+
+
+client.run('MTA4NzUyMDg2MTA4MDI2NDc3NA.G7Jt_9.L3zyXvNW7S-rilVqPAswTYcnhJ9fgf9eacarxw')
